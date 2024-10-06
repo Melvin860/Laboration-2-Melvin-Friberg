49bdcb97af5849cf9cd24ebfed86c87154a42ff7
Melvin860 <melvin.friberg@iths.se>
2024-10-06 20:21:55 +0200

    Add project files.

---
+++ Laboration 2 Melvin Friberg/Program.cs
@@ -0,0 +1,261 @@
+﻿namespace Laboration_2_Melvin_Friberg;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using Laboration_2_Melvin_Friberg;
+
+    internal class Program
+    {
+        static List<Kund> users = new List<Kund>();//Skapar en lista med alla kunder som finns registrerade
+        static void Main(string[] args)
+        {
+        
+            while (true)
+            {
+                
+                Console.WriteLine("1. Logga in");
+                Console.WriteLine("2. Skapa användare");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        LoggaIn();//Metod för inloggning
+                        break;
+                    case "2":
+                        SkapaAnvändare();//Metod för att skapa en användare
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltligt val, försök igen");
+                        break;
+                }
+            }
+
+            static void LoggaIn()//Logga in metod
+            {
+                Console.Clear();
+                Console.WriteLine("Skriv in användarnamn");
+                string username = Console.ReadLine();
+                Console.WriteLine("Skriv in ett Lösenord");
+                string password = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(username) || (string.IsNullOrWhiteSpace(password)))//Om det inte finns något värde i strängarna
+            {
+                Console.WriteLine("Du har inte angett något värde, försök igen");
+                Console.ReadLine();
+                Console.Clear();
+                return;
+            }
+            bool loginklar = false;//skapar en bool variabel som kollar så att värderna och inloggningen lyckades, hade bara massa if och else if men felmeddelandet kom när man loggade ut hela tiden, så jag fick skapa en bool variabel istället.
+            {
+                foreach (var user in users)
+                {
+                    if (user.Name == username && user.Password == password)//kollar så att inloggningen matchar med det som är sparat
+                    {
+                        Console.WriteLine($"Du är nu inloggad som {username}");
+                        Console.WriteLine("tryck 'Enter' för att gå vidare till butiken");
+                        Console.ReadLine();
+                        HuvudMeny(user);//Går till huvudmenyn
+                        loginklar = true;
+                        break;
+
+                    }
+                }
+                if (!loginklar)
+                {
+                    Console.WriteLine("Användarnamnet eller Lösenordet du angivit är fel, Försök gärna igen");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+
+                
+            }
+
+
+            static void SkapaAnvändare()//en metod för att skapa en användare
+            {
+                Console.Clear();
+                Console.WriteLine("Skriv in ditt användarnamn");
+                string name = Console.ReadLine();
+                Console.WriteLine("Skriv in ditt Lösenord");
+                string password = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(name) || (string.IsNullOrWhiteSpace(password)))//Om det inte finns något värde i strängarna
+            {
+                    Console.WriteLine("Användarnamn eller Lösenord kan inte vara tomt, Försök igen");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            foreach (var user in users)//Loopar igenom kund listan
+            {
+                if (user.Name.Equals(name, StringComparison.OrdinalIgnoreCase))//Kollar om det ny skapade användarnamnet är upptaget eller inte
+                {
+                    Console.WriteLine("Användarnamnet är upptaget, försök med något nytt");
+                    Console.ReadLine();
+                    Console.Clear();
+                    return;
+                }
+            }
+                var kund = new Kund(name, password);//Lägger till en ny kund i listan "Kund"
+                users.Add(kund);
+                Console.WriteLine($"Du har nu skapat ett nytt konto och ditt användarnamn är: {name}");
+                Console.WriteLine();
+                Console.WriteLine("Tryck 'Enter' för att Fortsätta");
+                Console.ReadLine();
+                Console.Clear();
+            
+                
+
+            }
+          
+
+            static void HuvudMeny(Kund user)//Huvudmenyn i användarens profil 
+            {
+           
+            decimal totalaPris = 0;//skapar en decimal för totalpriset
+            bool meny = true;
+                while (meny)
+                {
+                
+
+                    //----------V BUTIKEN V------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Hejsan  och välkommen till Melvins Livs!");
+                    Console.ResetColor();
+                    Console.WriteLine("Vänligen klicka på de alternativ som är bäst för dig");
+                    Console.WriteLine("1. Handla i Butiken");
+                    Console.WriteLine("2. Se kundvagnen");
+                    Console.WriteLine("3. Gå till kassan");
+                    Console.WriteLine("4. Logga ut");
+                    string input = Console.ReadLine();
+
+                    switch (input)// skapar upp en meny
+                    {
+
+                        case "1"://Handla i butiken
+                            bool innreMeny = true;
+                            while (innreMeny)//skapar en innre meny för att visa upp och handla på ett lätt sätt
+                            {
+                                List<Product> products = new List<Product>();//Gör en lista med alla produkter som finns
+                                Product banan = new Product("Banan", 15);
+                                Product pasta = new Product("Pasta", 20);
+                                Product mjölk = new Product("Mjölk", 25);
+                                products.Add(pasta);
+                                products.Add(banan);
+                                products.Add(mjölk);
+
+                                Console.Clear();
+                                Console.WriteLine("---------Butikens Varor---------");
+                                foreach (var product in products)//Skapar en foreach loop som ska skriva ut alla varor för användaren
+                                {
+                                    Console.WriteLine($"Produkten: {product.Name}, Pris: {product.Price}kr");//Skriver ut namn på produkterna och även pris
+                                }
+                                Console.WriteLine("-----------------------------------");
+                            Console.WriteLine("Dessa är alla produkter vi har för tillfället");
+                            Console.WriteLine("Välj produkt med hjälp av siffrorna på tangentbordet 1 Till 3");
+                            Console.WriteLine("1 = Längst upp");
+                            Console.WriteLine("3 = Längst ner");
+                            Console.WriteLine("Skriv back för att gå tillbaka");
+                                string köp = Console.ReadLine();
+                                switch (köp)//En köp funktion som fungerar som menyval
+                                {
+
+                                    case "1":
+                                        user.AddToCart(banan);//Lägger till banan i kundvagnen
+                                        Console.WriteLine($"1st Banan har lagts till i kundvagnen");
+                                        Console.WriteLine("Tryck 'Enter' för att gå vidare");
+                                        Console.ReadLine();
+                                        break;
+
+                                    case "2":
+                                        user.AddToCart(pasta);//Lägger till pasta i kundvagnen
+                                        Console.WriteLine($"1st Pasta har lagts till i kundvagnen");
+                                        Console.WriteLine("Tryck 'Enter' för att gå vidare");
+                                        Console.ReadLine();
+                                        break;
+                                    case "3":
+                                        user.AddToCart(mjölk);//Lägger till mjölk i kundvagnen
+                                        Console.WriteLine($"1st Mjölk har lagts till i kundvagnen");
+                                        Console.WriteLine("Tryck 'Enter' för att gå vidare");
+                                        Console.ReadLine();
+                                        break;
+
+                                    case "back"://avbryter menyn och går tillbaka till huvudmenyn
+                                        innreMeny = false;
+                                        break;
+
+                                    default:
+                                        Console.WriteLine("Ogiltligt val");//Felmeddelande
+                                        break;
+                                }
+                            }
+                            break;
+
+                        case "2"://Skriva ut Kundvagn
+                        var cart = user.GetCart();//anropar GetCart ifrån den inloggande "user"
+                        Console.Clear();
+                        Console.WriteLine(user.ToString());// skriver ut namn och lösenord på användaren
+                        Console.WriteLine("-----------------------------------");
+                        
+                            if (cart.Count == 0)//Om kundvagnen är tom 
+                                {
+                                    Console.WriteLine("Din kundvagn är tom.");
+                                }
+                                else
+                                Console.WriteLine("---Din kundvagn innehåller---");
+                            
+                                foreach (var product in cart)//Loopar igenom användarens Cart och kollar alla produkter i den
+                                {
+                                Console.WriteLine($"{product.Name}, {product.Price}kr");//Skriver ut både namn och pris
+                                }
+                                totalaPris = 0;//Sätter priset till 0 igen, annars ökar det för varje gång man går in
+
+                                for (int i = 0; i < cart.Count; i++)//En for loop som ska loopa igenom alla varor och räkna ut priset
+                                {
+                                var product = cart[i];
+                                totalaPris += product.Price;//beräknar kostnaden av allt
+                                }
+                                Console.WriteLine("---------------------");
+                                Console.WriteLine($"Totalt Pris: {totalaPris}kr");//Totala priset i kundvagnen
+                                Console.WriteLine("---------------------");
+                                Console.WriteLine("Tryck 'ENTER' för att gå tillbaka till Menyn");
+                                Console.ReadLine();
+                                break;
+                        case "3":
+                        Console.WriteLine("Välkommen till kassan!");
+                        Console.WriteLine($"Din totala kostnad är {totalaPris}kr ");
+                        Console.WriteLine("Hur vill du betala? Kort eller Kontant?");
+                        string betala = Console.ReadLine();
+                        if(betala.ToLower() == "kort")
+                        {
+                            Console.WriteLine("Sorry plånkan is at home");
+                        }
+                        else if (betala.ToLower() == "kontant")
+                        {
+                            Console.WriteLine("Du har inga pengar");
+                        }
+                        Console.ReadLine();
+                        break;
+                        case "4":
+                        Console.WriteLine("Är du säker på att du vill logga ut? y/n");//Ett val för att logga ut ifrån programmet
+                        string userinput = Console.ReadLine();
+                        if (userinput.ToLower() == "y")
+                        {
+                            meny = false;//Menyn blir falskt och återgår till logga in eller skapa användare
+                            Console.Clear();
+                        }
+                        else if (userinput.ToLower() == "n")
+                        {
+                            break;
+                        }
+                        break;
+                    default: Console.WriteLine("Ogiltligt val");
+                        break;
+                    }
+                
+
+                }
+            }
+        }
+    }
+
---
+++ Laboration 2 Melvin Friberg/grejer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboration_2_Melvin_Friberg
+{
+
+    class Kund//skapar en klass kund
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        private List<Product> Cart { get; set; }//en privat kundvagn för kunderna
+
+        public Kund(string name, string password)
+        {
+            Name = name;
+            Password = password;
+            Cart = new List<Product>();
+        }
+
+        //public List<Kund> Kunds { get; set; } = new List<Kund>();
+        public void AddToCart(Product product)//Gör en Addtocart metod som sparar produkter i kundernas carts
+        {
+            Cart.Add(product);
+        }
+        public List<Product> GetCart()//Skapar en getter som ska hämta innehållet ifrån kundvagnen
+        {
+            return Cart;
+        }
+        public override string ToString()//En Tostring metod som kommer skriva ut kundens inloggningsuppgifter i kundvagnen
+        {
+            string cartContents = Cart.Count > 0 ? string.Join(", ", Cart) : "Kundvagnen är tom";
+            return $"Kund Användarnamn: {Name}\nLösenord: {Password}";
+        }
+
+    }
+    
+    public class Product//Skapar en produkt klass som har namn ock pris
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
+
+
+
+
